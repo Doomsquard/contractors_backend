68a3752128868338f8f76d3a47544697888e661b
Doomsquard <zhilinsky99@gmail.com>
2023-05-24 21:42:15 +0300

    rep for backend

---
+++ README.md
@@ -0,0 +1,22 @@
+## Getting Started
+
+Req:
+
+postgres 14\15
+go 1.20
+
+## Create .env.local file
+
+```bash
+cp .env .env.local
+```
+
+change variables in env.local with server settings
+
+## Start local
+
+```bash
+go get .
+#Then
+go run .
+```
---
+++ main.go
@@ -0,0 +1,77 @@
+package main
+
+import (
+    "net/http"
+
+    "github.com/gin-gonic/gin"
+
+    "contractors_backend/db/connect.go"
+    "models/Users"
+    "github.com/joho/godotenv"
+    "log"
+)
+
+type constractor struct {
+    ID     string  `json:"id"`
+    Constractors_name string  `json:"constractors_name"`
+}
+
+var constractors = []constractor{
+    {
+        ID: "1", Constractors_name: "First", 
+    },
+    {
+        ID: "2", Constractors_name: "Second", 
+    },
+}
+
+func getConstractors(c *gin.Context) {
+    c.IndentedJSON(http.StatusOK, constractors)
+}
+
+func postConstractor(c *gin.Context) {
+    var newConstractor constractor
+
+    if err := c.BindJSON(&newConstractor); err != nil {
+        return
+    }
+
+    constractors = append(constractors, newConstractor)
+
+    c.IndentedJSON(http.StatusCreated, newConstractor)
+}
+
+func getConstractorById(c *gin.Context) {
+    id := c.Param("id")
+
+    for _, a := range constractors {
+        if a.ID == id {
+            c.IndentedJSON(http.StatusOK, a)
+            return
+        }
+    }
+    c.IndentedJSON(http.StatusNotFound, gin.H{"message": "constractor not found"})
+}
+
+func loadDatabase() {
+    database.Connect()
+}
+
+func loadEnv() {
+    err := godotenv.Load(".env.local")
+    if err != nil {
+        log.Fatal("Error loading .env file")
+    }
+}
+
+func main() {
+    loadEnv()
+    loadDatabase()
+
+    router := gin.Default()
+
+    router.POST("/constractors", postConstractor)
+    router.GET("/constractors", getConstractors)
+    router.GET("/constractors/:id", getConstractorById)
+    router.Run("localhost:8080")
+}
